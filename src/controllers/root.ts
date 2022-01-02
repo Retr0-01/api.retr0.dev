16be7e22f8fc0378adfd64e86e1ba6c4c1813bec
Retr0 <61121754+Retr0-01@users.noreply.github.com>
2022-01-02 21:18:17 +0200

    Deploy script

--- src/controllers/root.ts
+++ src/controllers/root.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import listEndpoints from "express-list-endpoints";
-import { app } from "../server";
+import { app } from "..";
 
 const getAllRoutes = async (req: Request, res: Response, next: NextFunction) =>
 {
