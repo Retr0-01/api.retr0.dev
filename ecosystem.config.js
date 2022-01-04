8d574e8e6ea9572cf28038b75d077a694c4f04d4
Retr0 <61121754+Retr0-01@users.noreply.github.com>
2022-01-04 19:26:27 +0200

    Configure pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports =
+{
+	apps :
+	[{
+		name: "api.spaceretr0.com",
+		script: "./build/index.js",
+		wait_ready: "true",
+		watch: true,
+		ignore_watch: [".vscode", ".github", "logs", "node_modules", "build", "db", "docs", "src"],
+		log_date_format: "DD-MM-YYYY HH:mm Z",
+		error_file: "./logs/error.log",
+		out_file: "./logs/output.log"
+	}]
+};
